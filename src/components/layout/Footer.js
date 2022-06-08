3c3e6f9730609b77a090f074abf813c076c8da74
Michael Xiang <14zx4@queensu.ca>
2022-06-08 16:58:23 -0400

    adding style

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,19 +1,21 @@
 import { Fragment } from 'react/cjs/react.production.min';
 import classes from './Footer.module.css';
 import { isMobile } from 'react-device-detect';
-
+import { Button } from '@mui/material';
 const Footer=()=>{
     return(
         <Fragment>
         <div className={isMobile?classes.media:classes.wrapper}>
-            <div>
-                <h4>Visit Our Location</h4>
+            <div className={classes.align}> 
+                <h4>Locations</h4>
+                <Button  sx={{fontSize:"1rem",fontWeight:"bold"}}>505 Highway 7 East</Button>
             </div>
-            <div>
-                <h4>Contact Us</h4>
+            <div className={classes.align}>
+                <h4>Contact Us </h4>
+                <Button sx={{fontSize:"1rem",fontWeight:"bold"}}>canloveada@gmail.com</Button>
             </div>
-            <div>
-                <h4>Hourse Of Operation</h4>
+            <div className={classes.align}>
+                <h4>Hours Of Operation</h4>
             </div>
         </div>
         <div>
